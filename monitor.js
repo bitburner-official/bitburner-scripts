529d935f467e827011497ba8753ecaf9d77cd6be
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-11-11 11:27:27 -0500

    Added some scripts

---
+++ monitor.js
@@ -0,0 +1,31 @@
+export async function main(ns) {
+    ns.tail();
+    const flags = ns.flags([
+        ['refreshrate', 200],
+    ])
+    if (flags._.length === 0) {
+        ns.tprint(`USAGE: run ${ns.getScriptName()} SERVER_NAME`);
+        return;
+    }
+    ns.disableLog('ALL');
+    while (true) {
+        const server = flags._[0];
+        let money = ns.getServerMoneyAvailable(server);
+        if (money === 0) money = 1;
+        const maxMoney = ns.getServerMaxMoney(server);
+        const minSec = ns.getServerMinSecurityLevel(server);
+        const sec = ns.getServerSecurityLevel(server);
+        ns.clearLog(server);
+        ns.print(`${server}:`);
+        ns.print(` $_______: ${ns.nFormat(money, "$0.000a")} / ${ns.nFormat(maxMoney, "$0.000a")} (${(money / maxMoney * 100).toFixed(2)}%)`);
+        ns.print(` security: +${(sec - minSec).toFixed(2)}`);
+        ns.print(` hack____: ${ns.tFormat(ns.getHackTime(server))} (t=${Math.ceil(ns.hackAnalyzeThreads(server, money))})`);
+        ns.print(` grow____: ${ns.tFormat(ns.getGrowTime(server))} (t=${Math.ceil(ns.growthAnalyze(server, maxMoney / money))})`);
+        ns.print(` weaken__: ${ns.tFormat(ns.getWeakenTime(server))} (t=${Math.ceil((sec - minSec) * 20)})`);
+        await ns.sleep(flags.refreshrate);
+    }
+}
+
+export function autocomplete(data, args) {
+    return data.servers;
+}
