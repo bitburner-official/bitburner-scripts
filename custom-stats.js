ec7a6444378ec2ef955bd504f1484f63aeec8cd4
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2021-12-12 14:46:19 -0400

    Sample script for how to add custom HUD stats

---
+++ custom-stats.js
@@ -0,0 +1,35 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    const args = ns.flags([["help", false]]);
+    if (args.help) {
+        ns.tprint("This script will enhance your HUD (Heads up Display) with custom statistics.");
+        ns.tprint(`Usage: run ${ns.getScriptName()}`);
+        ns.tprint("Example:");
+        ns.tprint(`> run ${ns.getScriptName()}`);
+        return;
+    }
+    
+    const doc = document; // This is expensive! (25GB RAM) Perhaps there's a way around it? ;)
+    const hook0 = doc.getElementById('overview-extra-hook-0');
+    const hook1 = doc.getElementById('overview-extra-hook-1');
+    while (true) {
+        try {
+            const headers = []
+            const values = [];
+            // Add script income per second
+            headers.push("ScrInc");
+            values.push(ns.getScriptIncome()[0].toPrecision(5) + '/sec');
+            // Add script exp gain rate per second
+            headers.push("ScrExp");
+            values.push(ns.getScriptExpGain().toPrecision(5) + '/sec');
+            // TODO: Add more neat stuff
+
+            // Now drop it into the placeholder elements
+            hook0.innerText = headers.join(" \n");
+            hook1.innerText = values.join("\n");
+        } catch (err) { // This might come in handy later
+            ns.print("ERROR: Update Skipped: " + String(err));
+        }
+        await ns.sleep(1000);
+    }
+}
