c56bb0b1520a06f6750d889eec27a5e3932e4efe
GreenMan36 <Jaspervandenbremer@gmail.com>
2021-12-25 22:38:28 +0100

    Correction
    Simply adding a check for the server growth ofc fixes the real issue.
    Then adding 1 to both max and current money will keep the % unaffected while allowing all calculations to happen and then you -1 to whats being displayed so everything is alright.
    No `/0` and also no random 1 buck.
    
    Server money is at 100% which is correct because `0/0 = 100%` just like `1/1 = 100%`

--- monitor.js
+++ monitor.js
@@ -14,18 +14,16 @@ export async function main(ns) {
     ns.disableLog('ALL');
     while (true) {
         const server = flags._[0];
-        let money = ns.getServerMoneyAvailable(server);
-        const maxMoney = ns.getServerMaxMoney(server);
+        let money = ns.getServerMoneyAvailable(server)+1;
+        let maxMoney = ns.getServerMaxMoney(server)+1;
         const minSec = ns.getServerMinSecurityLevel(server);
         const sec = ns.getServerSecurityLevel(server);
         ns.clearLog(server);
         ns.print(`${server}:`);
-        if (money < 1) {
-            ns.print(` $_______: ${ns.nFormat(money, "$0.000a")} / ${ns.nFormat(maxMoney, "$0.000a")} (ERROR MONEY IS ${money})`);
-        } else { ns.print(` $_______: ${ns.nFormat(money, "$0.000a")} / ${ns.nFormat(maxMoney, "$0.000a")} (${(money / maxMoney * 100).toFixed(2)}%)`) }
+        ns.print(` $_______: ${ns.nFormat(money-1, "$0.000a")} / ${ns.nFormat(maxMoney-1, "$0.000a")} (${((money / maxMoney * 100).toFixed(2))}%)`);
         ns.print(` security: +${(sec - minSec).toFixed(2)}`);
         ns.print(` hack____: ${ns.tFormat(ns.getHackTime(server))} (t=${Math.ceil(ns.hackAnalyzeThreads(server, money))})`);
-        if (money < 1) {
+        if (ns.getServerGrowth(server) < 1) {
             ns.print(` grow____: ${ns.tFormat(ns.getGrowTime(server))} (ERROR GROWTH IS 0)`);
         } else { ns.print(` grow____: ${ns.tFormat(ns.getGrowTime(server))} (t=${Math.ceil(ns.growthAnalyze(server, maxMoney / money))})`) }
         ns.print(` weaken__: ${ns.tFormat(ns.getWeakenTime(server))} (t=${Math.ceil((sec - minSec) * 20)})`);
